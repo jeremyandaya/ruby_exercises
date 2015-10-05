1cffac83be69908006bb6dfd393406b94ff2400d
Jeremy Andaya <jeremy.andaya@gmail.com>
2015-10-05 14:44:12 -0400

    first commit

---
+++ hello_ruby.rb
@@ -0,0 +1,49 @@
+# Create a repository and push the ruby exercises you've completed today. 
+# Submit the GitHub link for a grade.
+
+# In your ruby file
+
+# Creating and outputting variables
+x = 2
+y = 9
+puts "The number #{x} is less than #{y}"
+
+# Nesting an Array within another Array
+
+nested_array = [["hello", "world"], ["hi", "ruby"]]
+
+# Assign hashes to variables
+
+names = Hash.new
+names[1] = "Tony Stark"
+names[2] = "Bruce Banner"
+
+puts names
+
+# Utilizing loops for mathematical equations 
+
+number = 0 
+while number < 5 
+  puts "This is my current number: #{number}"
+  
+  number = number + 1
+end
+
+# Create a function to add a string to a variable
+
+def addString(arg)
+	var1 = arg
+end
+puts addString("Add this string to variable var1")
+
+
+# Create a function to differentiate number elements in an array
+
+# Create a function that takes arrays and a function will combine them
+
+# Create a loop to print numbers from x to y.
+
+# Create a loop that loops through all the numbers from x to y and passes a condition. 
+# 	If the condition is met, a string will be printed. 
+
+
---
+++ more_advance_ruby.rb
@@ -0,0 +1,18 @@
+# In your Ruby file
+
+# Storing an information into an instance variable, which is outputted when an instance is created
+
+# Create an object with an instance and class method
+
+# Create an Object with attributes and an instance method. Assign it to a variable, then change the attributes and call the method.
+
+# Build a program that analyzes the input supplied by a heredoc then return a hash o information.
+
+# Build an object 'Zoo' with the attributes 'Exhubit'. The attribute includes an array of object instances.
+
+# Build an adventure game with your knowledge of objects . Store characters with classes and attributes.
+
+# Create a class that can store values and apply mathematical equations(calculator)
+
+# Create an elevator instance with an up and down method. It will output a greeting and the current floor you are on.
+
