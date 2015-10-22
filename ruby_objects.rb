89cb281b4be09648ef49a3f4ec9ee2d106be6132
Jeremy Andaya <jeremy.andaya@gmail.com>
2015-10-22 12:03:06 -0400

    added elevator class

--- ruby_objects.rb
+++ ruby_objects.rb
@@ -27,3 +27,36 @@ Calculator.new.add(3, 4)
 Calculator.new.subtract(10, 4)
 Calculator.new.multiply(20, 4)
 Calculator.new.divide(208, 11)
+
+
+
+# ELEVATOR
+class Elevator
+
+	def destination_floor
+	end
+
+	def up
+	end
+
+	def down
+	end
+
+end
+
+floor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+current_floor = floor[2]
+puts "Current floor => #{current_floor}"
+
+
+
+
+
+
+
+
+
+
+
+
+
