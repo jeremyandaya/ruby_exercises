50daa7eb071063ed40698d5765c2badd39f60acb
Jeremy Andaya <jeremy.andaya@gmail.com>
2015-10-31 10:49:19 -0400

    updated more advanced ruby exercise

--- more_advanced_ruby.rb
+++ more_advanced_ruby.rb
@@ -1,14 +1,33 @@
 # In your Ruby file
 
 # Storing an information into an instance variable, which is outputted when an instance is created
+class Foobar
+	def set_foo(foo)
+		@foo = foo
+	end
 
-# Create an object with an instance and class method
-class Reptile
-	def initialize
-		puts "slimy"
+	def set_bar(bar)
+		@bar = bar
 	end
 end
-Reptile.new
+
+result = Foobar.new
+puts result.set_foo("FOO") + result.set_foo("BAR")
+
+# Create an object with an instance and class method
+class Person
+	def initialize(name, age, gamer)
+		@name = name
+		@age = age
+		@gamer = gamer
+	end
+
+	def to_s
+		"The person's name is #{@name}. He is #{@age} years old and is a #{@gamer} gamer."
+	end
+end
+
+puts Person.new("Gandhi", 88, true)
 
 # Create an Object with attributes and an instance method. Assign it to a variable, then change the attributes and call the method.
 
@@ -19,6 +38,33 @@ Reptile.new
 # Build an adventure game with your knowledge of objects . Store characters with classes and attributes.
 
 # Create a class that can store values and apply mathematical equations(calculator)
+class Calculator
+	def add(value1, value2)
+		result = value1 + value2
+		puts "Addition result is: #{result}"
+	end
+
+	def subtract(value1, value2)
+		result = value1 - value2
+		puts "Subtraction result is: #{result}"
+	end
+
+	def multiply(value1, value2)
+		result = value1 * value2
+		puts "Multiplication result is: #{result}"
+	end
+
+	def divide(value1, value2)
+		result = value1 / value2
+		remainder = value1 % value2
+		puts "Division result is: #{result} with a remainder of #{remainder}"
+	end
+end
+
+Calculator.new.add(3, 4)
+Calculator.new.subtract(10, 4)
+Calculator.new.multiply(20, 4)
+Calculator.new.divide(208, 11)
 
 # Create an elevator instance with an up and down method. It will output a greeting and the current floor you are on.
 
--- ruby_objects.rb
+++ ruby_objects.rb
@@ -1,52 +1,70 @@
 # CALCULATOR
 
-class Calculator
-	def add(value1, value2)
-		result = value1 + value2
-		puts "Addition result is: #{result}"
-	end
+# class Calculator
+# 	def add(value1, value2)
+# 		result = value1 + value2
+# 		puts "Addition result is: #{result}"
+# 	end
 
-	def subtract(value1, value2)
-		result = value1 - value2
-		puts "Subtraction result is: #{result}"
-	end
+# 	def subtract(value1, value2)
+# 		result = value1 - value2
+# 		puts "Subtraction result is: #{result}"
+# 	end
 
-	def multiply(value1, value2)
-		result = value1 * value2
-		puts "Multiplication result is: #{result}"
-	end
+# 	def multiply(value1, value2)
+# 		result = value1 * value2
+# 		puts "Multiplication result is: #{result}"
+# 	end
 
-	def divide(value1, value2)
-		result = value1 / value2
-		remainder = value1 % value2
-		puts "Division result is: #{result} with a remainder of #{remainder}"
-	end
-end
+# 	def divide(value1, value2)
+# 		result = value1 / value2
+# 		remainder = value1 % value2
+# 		puts "Division result is: #{result} with a remainder of #{remainder}"
+# 	end
+# end
 
-Calculator.new.add(3, 4)
-Calculator.new.subtract(10, 4)
-Calculator.new.multiply(20, 4)
-Calculator.new.divide(208, 11)
+# Calculator.new.add(3, 4)
+# Calculator.new.subtract(10, 4)
+# Calculator.new.multiply(20, 4)
+# Calculator.new.divide(208, 11)
 
 
 
 # ELEVATOR
+# class Elevator
+
+# 	def destination_floor
+# 	end
+
+# 	def up
+# 	end
+
+# 	def down
+# 	end
+
+# end
+
+# floor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# current_floor = floor[2]
+# puts "Current floor => #{current_floor}"
+
+
 class Elevator
-
-	def destination_floor
+	def initialize current_floor, destination_floor
+		@current_floor = current_floor
+		@destination_floor = destination_floor
 	end
 
-	def up
+	def floor
+		"#{@current_floor} #{@destination_floor}"
 	end
-
-	def down
-	end
-
 end
 
-floor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-current_floor = floor[2]
-puts "Current floor => #{current_floor}"
+elevator1 = Elevator.new
+elevator.current_floor = 1
+elevator.destination_floor = 4
+
+puts elevator1(1, 4)
 
 
 
